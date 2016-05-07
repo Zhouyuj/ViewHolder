6de97eac9ef4f3a22f305c48ba73ef5dee8e646d
Zhouyuj <970590239@qq.com>
2016-05-07 16:45:41 +0800

    add i

---
+++ Test.java
@@ -0,0 +1,21 @@
+package com.zhou.asus;
+
+import java.util.ArrayList;
+
+/**
+ * Created by asus on 2016/5/7.
+ */
+public class Test {
+
+    Test(){
+        ArrayList<String> a = new ArrayList<>();
+    }
+
+    public static void main(String[] args){
+       System.out.print("test");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("test"+i);
+
+        }
+    }
+}
